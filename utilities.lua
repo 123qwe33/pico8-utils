53edc0360305cd6b86aae8521b344e83c5d25ea4
Geoff Rogers <geoffrogersdesign@gmail.com>
2025-07-06 19:48:25 -0400

    Now Dumping to 'log.txt' In Project Root

--- utilities.lua
+++ utilities.lua
@@ -3,11 +3,11 @@ function dump(t, indent)
   for k, v in pairs(t) do
     local kstr = "["..tostr(k).."]"
     if type(v) == "table" then
-      printh(indent..kstr.." = {")
+      printh(indent..kstr.." = {", 'log.txt')
       dump(v, indent.."  ")
-      printh(indent.."}")
+      printh(indent.."}", 'log.txt')
     else
-      printh(indent..kstr.." = "..tostr(v))
+      printh(indent..kstr.." = "..tostr(v), 'log.txt')
     end
   end
 end
