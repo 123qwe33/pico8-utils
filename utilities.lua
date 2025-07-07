241636d9916b741a585e054135fd324567302b95
Geoff Rogers <geoffrogersdesign@gmail.com>
2025-07-07 12:52:01 -0400

    Using Native Format

--- utilities.lua
+++ utilities.lua
@@ -3,11 +3,11 @@ function dump(t, indent)
   for k, v in pairs(t) do
     local kstr = "["..tostr(k).."]"
     if type(v) == "table" then
-      printh(indent..kstr.." = {", 'log.log')
+      printh(indent..kstr.." = {", 'log.p8l')
       dump(v, indent.."  ")
-      printh(indent.."}", 'log.log')
+      printh(indent.."}", 'log.p8l')
     else
-      printh(indent..kstr.." = "..tostr(v), 'log.log')
+      printh(indent..kstr.." = "..tostr(v), 'log.p8l')
     end
   end
 end
